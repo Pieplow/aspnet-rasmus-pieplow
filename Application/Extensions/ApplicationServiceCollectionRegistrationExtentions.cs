166648af6e3e07a32e058f8a74277503b47d86c5
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2026-03-24 10:25:51 +0100

    first commit

---
+++ Application/Extensions/ApplicationServiceCollectionRegistrationExtentions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Application.Extensions;
+
+public static class ApplicationServiceCollectionRegistrationExtentions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration, IHostEnvironment env)
+    {
+        return services;
+    }
+}
---
+++ Infrastructure/Extensions/InfrastructureServiceCollectionRegistrationExtentions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Infrastructure.Extensions;
+
+public static class InfrastructureServiceCollectionRegistrationExtentions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, IHostEnvironment env)
+      {
+      return services;
+      }
+ }
