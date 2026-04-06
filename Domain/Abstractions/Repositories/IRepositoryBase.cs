6b0baa546974b69dcfb1e2eb8a05ddaf6d5da1df
Rasmus Pieplow <rasmus.pieplow@gmail.com>
2026-04-06 16:25:38 +0200

    added RepositoryBase

---
+++ Domain/Abstractions/Repositories/IRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Persistence.Context.Extensions
+{
+    public interface IRepositoryBase<TDomainModel, TId>
+    {
+        Task AddAsync(TDomainModel model, CancellationToken ct = default);
+        Task<IReadOnlyList<TDomainModel>> GetAllAsync(CancellationToken ct = default);
+        Task<TDomainModel?> GetByIdAsync(TId id, CancellationToken ct = default);
+        Task<bool> RemoveAsync(TDomainModel model, CancellationToken ct = default);
+        Task<bool> UpdateAsync(TDomainModel model, CancellationToken ct = default);
+    }
+}
--- Domain/Enteties/Booking.cs
+++ Domain/Enteties/Booking.cs
@@ -1,8 +1,13 @@
 ﻿
-
-namespace Domain.Enteties
+namespace Domain.Enteties;
+public class Booking
 {
-    internal class Booking
+    public Guid UserId { get; private set; }
+    public Guid GymClassId { get; private set; }
+
+    public Booking(Guid userId, Guid gymClassId)
     {
+        UserId = userId;
+        GymClassId = gymClassId;
     }
 }
